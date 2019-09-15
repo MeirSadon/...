caab8e9455410a1cad2de7049824ba1715805dee
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-09-15 17:48:23 +0300

    Update FlyingCenterConfig.cs

--- FlightManagementProject/FlightManagementProject/Facade/FlyingCenterConfig.cs
+++ FlightManagementProject/FlightManagementProject/Facade/FlyingCenterConfig.cs
@@ -12,10 +12,10 @@ namespace FlightManagementProject.Facade
         public const string ADMIN_PASSWORD = "9999";
 
         // Azure Connection.
-        //public const string CONNECTION_STRING = @"Server=tcp:flight-managment-srv.database.windows.net,1433;Initial Catalog=FlightManagmentDB;Persist Security Info=False;User ID=meir;Password=Password1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const string CONNECTION_STRING = @"Server=tcp:flight-managment-srv.database.windows.net,1433;Initial Catalog=FlightManagmentDB;Persist Security Info=False;User ID=meir;Password=Password1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         
         // My Computer Connection.
-        public const string CONNECTION_STRING = @"Data Source=MEIR-PC\SQLEXPRESS ;Initial Catalog = FlightManagement; Integrated Security = true";
+        //public const string CONNECTION_STRING = @"Data Source=MEIR-PC\SQLEXPRESS ;Initial Catalog = FlightManagement; Integrated Security = true";
 
         public const int TIMEFORTHREADHISTORY = 1000 * 3600 * 24;
 
